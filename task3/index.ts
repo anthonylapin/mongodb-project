c79a540a6a16884b4a4a798bb307781e2c983606
Anton Lapin <antonlapin@Antons-MacBook-Pro.local>
2020-10-08 17:12:57 +0200

    regexp task3 fixed

--- task3/index.ts
+++ task3/index.ts
@@ -5,14 +5,16 @@ async function getCourses() {
   try {
     const courses = await Course.find({
       isPublished: true,
-    }).or([
-      {
-        price: { $gte: 15 },
-      },
-      {
-        name: "/*.by.*/i",
-      },
-    ]);
+    })
+      .or([
+        {
+          price: { $gte: 15 },
+        },
+        {
+          name: /.*by.*/i,
+        },
+      ])
+      .select({ name: 1, price: 1, isPublished: 1, _id: 0 });
     return courses;
   } catch (error) {
     throw new Error(error);
